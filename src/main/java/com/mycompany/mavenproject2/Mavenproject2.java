3853911a97d21fe3574c0c3fdde58cfa08e72738
User <User@erg1-pc12>
2022-11-11 16:52:14 +0200

    first lines

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author User
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        String name; tod the employee's name 
+        int age; //to hold the employee's age 
+        char gender; //To hold employee's gender
+        double salary; // To hold employee's age
+        // create a scanner object to read input
+}
